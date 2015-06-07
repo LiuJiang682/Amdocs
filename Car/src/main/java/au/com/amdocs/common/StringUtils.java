12cc6559c42ac487f94a4726e9127fbfb02f98df
Jiang Liu <liujiang682@gmail.com>
2015-06-07 16:20:16 +1000

    Fifth check in.

--- Car/src/main/java/au/com/amdocs/common/StringUtils.java
+++ Car/src/main/java/au/com/amdocs/common/StringUtils.java
@@ -1,10 +1,12 @@
 package au.com.amdocs.common;
 
-
-
 public class StringUtils {
 	
 	public static boolean isNullOrEmpty(final String string) {
 		return (string == null)||(string.length() == CommonConstants.ZERO);
+	}
+	
+	public static boolean isNullOrEmptyArray(final String[] strings) {
+		return ((strings == null) || (strings.length == CommonConstants.ZERO));
 	}
 }
